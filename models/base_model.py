d01523fe011e63396c7c005540b8b650c8cb2028
LiantsoaANDR <liantsoa.cpro@gmail.com>
2024-01-12 09:12:12 -0800

    AttributeError:  type object 'FileStorage' has no attribute '_FileStorage__objetcs'

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 Module for our base of all other class models in this project
 """
 from datetime import datetime
-from models import storage
+import models
 import uuid
 
 
@@ -38,7 +38,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
             self.updated_at = datetime.today()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -53,7 +53,7 @@ class BaseModel:
         with the current datetime
         """
         self.updated_at = datetime.today()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
